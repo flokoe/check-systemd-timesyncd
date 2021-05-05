232997f03187e226ea57dfed5795e2852a7bac99
flokoe <koehler.florian@gmx.de>
2021-05-05 16:26:32 +0200

    Bump version

--- check_timesyncd.py
+++ check_timesyncd.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser
 import sys, subprocess, re
 
 __author__  = 'Florian Köhler'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __license__ = 'MIT'
 
 def to_dict(list):
